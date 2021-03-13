42810eb9c0ba390e9553fdf016afef473df1a05c
sebin <sebin.francis@cognizant.com>
2021-03-13 17:20:23 -0500

    added createdmany functions

--- src/main/java/com/example/zookeeper/AnimalService.java
+++ src/main/java/com/example/zookeeper/AnimalService.java
@@ -2,6 +2,9 @@ package com.example.zookeeper;
 
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class AnimalService {
 
@@ -15,5 +18,16 @@ public class AnimalService {
         animalRepository.save(
                 new AnimalEntity(animalDto.getName(), animalDto.getType())
         );
+    }
+
+    public void createMany(List<AnimalDto> animals) {
+
+        animalRepository.saveAll(animals
+                .stream()
+                .map(animal -> {
+                    return new AnimalEntity(animal.getName(), animal.getType());
+                })
+                .collect(Collectors.toList())
+                );
     }
 }
--- src/test/java/com/example/zookeeper/AnimalServiceTest.java
+++ src/test/java/com/example/zookeeper/AnimalServiceTest.java
@@ -6,6 +6,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.verify;
 
 @ExtendWith(MockitoExtension.class)
@@ -25,6 +28,23 @@ public class AnimalServiceTest {
         verify(mockAnimalRepository).save(
                 new AnimalEntity("Tiger", "walking")
         );
-
     }
+
+    @Test
+    public void createMany() {
+        List<AnimalDto> animals = new ArrayList<>();
+
+        animals.add( new AnimalDto("Tiger", "walking"));
+        animals.add( new AnimalDto("Tuna", "swimming"));
+
+        List<AnimalEntity> entities = new ArrayList<>();
+        entities.add(new AnimalEntity("Tiger", "walking"));
+        entities.add(new AnimalEntity("Tuna", "swimming"));
+        subject.createMany(animals);
+        verify(mockAnimalRepository).saveAll(
+                entities
+        );
+    }
+
+
 }
